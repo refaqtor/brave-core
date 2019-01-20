2f55dbe188b597e9a0cc851e742b4d76b178adb5
Terry Mancey <terry.mancey@icloud.com>
2019-01-20 13:04:24 +0000

    Add support to trigger Confirmations OnTimer

--- vendor/bat-native-ledger/src/ledger_impl.cc
+++ vendor/bat-native-ledger/src/ledger_impl.cc
@@ -1260,6 +1260,7 @@ void LedgerImpl::KillTimer(uint32_t timer_id) {
 }
 
 void LedgerImpl::OnConfirmationsTimer(uint32_t timer_id) {
+  bat_confirmations_->OnTimer(timer_id);
 }
 
 std::unique_ptr<confirmations::LogStream> LedgerImpl::Log(
